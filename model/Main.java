91d58ed101e1ac7fa2b23f0e8ad273b8c29695f9
Victor <victor.oliveira4942081@edu.pe.senac.br>
2025-07-30 09:27:44 -0300

    Merge feito com Cabral, sucesso

--- model/Main.java
+++ model/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Aluno aluno = new Aluno("Victor Gustavo", "12/05/2007", "n. 9177393", "Mecânico");
+        String turma;
+        Aluno aluno = new Aluno("Victor Gustavo", "12/05/2007", "n. 9177393", "Mecânico", "1º Ano");
         aluno.exibirDados();
 
         Professor professor = new Professor("Dr. Cabral", "29/12/1980", "Projeto de Vida");
