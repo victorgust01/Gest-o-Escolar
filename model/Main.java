e7ff140087828b9769edd2fce7c3684dcfeb73ac
Victor <victor.oliveira4942081@edu.pe.senac.br>
2025-07-30 10:59:06 -0300

    Atualização Main.java by VictorGust

--- model/Main.java
+++ model/Main.java
@@ -1,10 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        String turma;
+        
         Aluno aluno = new Aluno("Victor Gustavo", "12/05/2007", "n. 9177393", "Mecânico", "1º Ano");
         aluno.exibirDados();
 
+        Aluno aluno2 = new Aluno("Pililiu", "25/09/2006", "n. 1995217", "Engenharia", "3º Ano");
+        aluno2.exibirDados();
+
         Professor professor = new Professor("Dr. Cabral", "29/12/1980", "Projeto de Vida");
         professor.exibirDados();
+
+        Professor professor2 = new Professor("Moscou", "17/03/2025", "Física");
+        professor2.exibirDados();
+
+        Disciplina disciplina = new Disciplina("Matemática", "Século VI a.C.", "Álgebra");
+        disciplina.exibirDados();
+
+        Disciplina disciplina2 = new Disciplina("História", "Século XX a.C.", "História Geral");
+        disciplina2.exibirDados();
+
+        
     }
 }
