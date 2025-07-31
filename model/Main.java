8a743871741daf09d3da91b48a2aec4da2b98f0c
Victor <victor.oliveira4942081@edu.pe.senac.br>
2025-07-31 08:18:14 -0300

    Atualização dados

--- model/Main.java
+++ model/Main.java
@@ -12,6 +12,16 @@ public class Main {
         aluno4.exibirDados();
         Aluno aluno5 = new Aluno("Luan", "n. 1122334", "Química", "2º Ano");
         aluno5.exibirDados();
+        Aluno aluno6 = new Aluno("Gustavo", "n. 9988776", "Engenharia de Software", "3º Ano");
+        aluno6.exibirDados();
+        Aluno aluno7 = new Aluno("João", "n. 1231231", "Ciência da Computação", "1º Ano");
+        aluno7.exibirDados();
+        Aluno aluno8 = new Aluno("Maria", "n. 4564564", "Engenharia Civil", "2º Ano");  
+        aluno8.exibirDados();
+        Aluno aluno9 = new Aluno("Ana", "n. 7897897", "Arquitetura", "3º Ano");
+        aluno9.exibirDados();
+        Aluno aluno10 = new Aluno("Pedro", "n. 3213213", "Design Gráfico", "1º Ano");
+        aluno10.exibirDados();
 
         System.out.println("------------------------------- PROFESSORES -------------------------------");
         Professor professor = new Professor("Dr. Cabral", "Projeto de Vida");
@@ -24,6 +34,16 @@ public class Main {
         professor4.exibirDados();
         Professor professor5 = new Professor("Victor Gustavo", "Engenharia de Software");
         professor5.exibirDados();
+        Professor professor6 = new Professor("Gustavo", "Ciência da Computação");
+        professor6.exibirDados();
+        Professor professor7 = new Professor("João", "Engenharia Civil");   
+        professor7.exibirDados();
+        Professor professor8 = new Professor("Maria", "Arquitetura");
+        professor8.exibirDados();
+        Professor professor9 = new Professor("Ana", "Design Gráfico");
+        professor9.exibirDados();
+        Professor professor10 = new Professor("Pedro", "Projeto de Vida");
+        professor10.exibirDados();
 
         System.out.println("------------------------------- DISCIPLINAS -------------------------------");
         Disciplina disciplina = new Disciplina("Matemática", "Álgebra");
@@ -36,6 +56,16 @@ public class Main {
         disciplina4.exibirDados();
         Disciplina disciplina5 = new Disciplina("Química", "Química Orgânica");
         disciplina5.exibirDados();
+        Disciplina disciplina6 = new Disciplina("Biologia", "Genética");
+        disciplina6.exibirDados();
+        Disciplina disciplina7 = new Disciplina("Literatura", "Literatura Brasileira");
+        disciplina7.exibirDados();
+        Disciplina disciplina8 = new Disciplina("Arte", "História da Arte");
+        disciplina8.exibirDados();
+        Disciplina disciplina9 = new Disciplina("Educação Física", "Atividades Físicas");
+        disciplina9.exibirDados();
+        Disciplina disciplina10 = new Disciplina("Informática", "Programação Básica");
+        disciplina10.exibirDados();
 
         
     }
